ca48d8fb72eb08f26a8a9dbcac9fd7cfc961e6e4
alexmoiseyev <keysergame24@gmail.com>
2025-06-26 17:04:47 +0300

    fix/изменен handler.php, input type="checkbox" и select в формах

--- chapter8/handler.php
+++ chapter8/handler.php
@@ -1,12 +1,5 @@
 <?php
-if($_SERVER['REQUEST_METHOD'] == 'GET'){
-    $_GET['checkbox'] = isset($_GET['checkbox']) ? $_GET['checkbox'] : '111';
-    echo "GET checkbox=".$_GET['checkbox']."<br>";
-}
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $_POST['checkbox'] = isset($_POST['checkbox']) ? $_POST['checkbox'] : '111';
-    echo "POST checkbox=".$_POST['checkbox'] ."<br>";
-}
+
 echo '<br>';
 echo 'GET массив: ';
 echo "<pre>";
