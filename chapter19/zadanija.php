6c405dc9b9afcc95f15bdf8b0c6896e82c2995b8
alexmoiseyev <keysergame24@gmail.com>
2025-07-10 12:55:44 +0300

    chapter19-fix/задание 2 добавлен вывод месяца, недели , добавлен вывод для $timestamp = 1611337801

--- chapter19/zadanija.php
+++ chapter19/zadanija.php
@@ -10,17 +10,19 @@ echo $timestamp;
 echo "<br>";
 
 // ЗАДАНИЕ 2 // 
-
-$timestamp = 1578652367;
-$date = date('d.m.Y H:i:s', $timestamp);
-echo $date;
-echo "<br>";
-$timestamp = 1578652367;
-$dateString = date('d.m.Y H:i:s', $timestamp);  //strftime устарела php 8.1
-echo $dateString;  
-echo "<br>";
+    //1)
+    $timestamp = 1578652367;
+    $dateStr = date('d F l Y H:i:s', $timestamp); //strftime устарела php 8.1
+    echo $dateStr;
+    echo "<br>";
+    //2)
+    $timestamp = 1611337801;
+    $dateStr = date('d F l Y H:i:s', $timestamp);  //strftime устарела php 8.1
+    echo $dateStr;
+    echo "<br>";
 
 // ЗАДАНИЕ 3 //
+
 $startDate = new DateTime('-1000-01-01'); 
 $endDate = new DateTime('9000-01-01');
 $interval = $startDate->diff($endDate);
